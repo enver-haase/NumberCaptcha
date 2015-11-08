bee30871476a97e9415a1541c51f57803e2257dd
Joonas Lehtinen <joonas@vaadin.com>
2015-11-08 11:57:18 +0200

    Fixed JavaScript loading problems

--- src/main/java/com/vaadin/tutorial/bulletchart/com_vaadin_tutorial_bulletchart_BulletChart.js
+++ src/main/java/com/vaadin/tutorial/bulletchart/com_vaadin_tutorial_bulletchart_BulletChart.js
@@ -15,12 +15,13 @@ com_vaadin_tutorial_bulletchart_BulletChart = function() {
 
 
         function exampleData() {
-          return {
-          	"title":"Revenue",		//Label the bullet chart
-          	"subtitle":"US$, in thousands",		//sub-label for bullet chart
-          	"ranges":[150,225,300],	 //Minimum, mean and maximum values.
-          	"measures":[220],		 //Value representing current measurement (the thick blue line in the example)
-          	"markers":[250]			 //Place a marker on the chart (the white triangle marker)
-          };
+            return {
+                "title": "Revenue",		//Label the bullet chart
+                "subtitle": "US$, in thousands",		//sub-label for bullet chart
+                "ranges": [150, 225, 300],	 //Minimum, mean and maximum values.
+                "measures": [220],		 //Value representing current measurement (the thick blue line in the example)
+                "markers": [250]			 //Place a marker on the chart (the white triangle marker)
+            }
+        };
 
 };
